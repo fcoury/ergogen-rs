0e6324ae4eb799d99ea0be67b8ef39fbb33535c3
Felipe Coury <felipe.coury@gmail.com>
2025-02-15 12:03:54 -0300

    feat: initial web app setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _, EnvFilter};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Starts the web interface
+    Web {
+        #[clap(short, long)]
+        listen_addr: Option<String>,
+
+        #[clap(short, long)]
+        port: Option<u16>,
+    },
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+            // Default to INFO level if RUST_LOG is not set
+            "ergogen=info,tower_http=info".into()
+        }))
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()?;
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Web { listen_addr, port } => {
+            ergogen_app::start(listen_addr, port).await?;
+        }
+    }
+
+    Ok(())
 }
